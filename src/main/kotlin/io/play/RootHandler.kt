8b8b18e84c6c6e6ddfbf90f9df0596695f37d851
Gerard Downes <gerard.downes@hexlabs.io>
2019-01-04 22:50:39 +0000

    Moving library & environment initialization logic to when required

--- src/main/kotlin/io/play/RootHandler.kt
+++ src/main/kotlin/io/play/RootHandler.kt
@@ -34,20 +34,6 @@ val root = Filter{ next -> { it ->
 }.then(ServerFilters.Cors(CorsPolicy.UnsafeGlobalPermissive)).then(RootHandler)
 
 object LambdaHandler: AppLoader{
-    init {
-        println("Checking tmp directory")
-        val libs = File("/tmp")?.listFiles()?.filter { it.name.endsWith(".jar") }?.toList() ?: emptyList()
-        println("Found the following libs: $libs")
-        if(libs.isEmpty()){
-            val bucket = "kotlin-playground-server-libs"
-            val client =  AmazonS3Client.builder().withRegion("eu-west-1").build()
-            val items = client.listObjects(bucket).objectSummaries
-            items.forEach{
-                println("Downloading ${it.key}")
-                client.getObject(GetObjectRequest(bucket, it.key), File("/tmp/${it.key}"))
-            }
-        }
-    }
     override fun invoke(p1: Map<String, String>) = root
 }
 
@@ -56,21 +42,39 @@ object RootHandler: HttpHandler{
     private val highlightResult = Body.auto<Map<String, List<ErrorHandler.ErrorDescriptor>>>().toLens()
     private val completionResult = Body.auto<List<CompletionProvider.CompletionVariant>>().toLens()
     override fun invoke(request: Request): Response {
-        EnvironmentManager.createEnvironment()
         val type = request.query("type")
         return when(type){
             "getKotlinVersions" -> Response(OK).body("[{\"version\":\"1.3.0\",\"build\":\"1.3.0\",\"obsolete\":false,\"latestStable\":true,\"hasScriptJar\":false,\"stdlibVersion\":\"1.3.0\"}]")
-            "highlight" -> Response(OK).with(
-                    highlightResult of RunHandler.highlight(projectFrom(request))
-            )
-            "complete" -> Response(OK).with(
-                    completionResult of RunHandler.complete(projectFrom(request), lineFrom(request), characterFrom(request))
-            )
-            "run" -> Response(OK).with(
-                    result of RunHandler.run(projectFrom(request))
-            )
+            "highlight" -> {
+                setupEnvironment()
+                Response(OK).with(highlightResult of RunHandler.highlight(projectFrom(request)))
+            }
+            "complete" -> {
+                setupEnvironment()
+                Response(OK).with(completionResult of RunHandler.complete(projectFrom(request), lineFrom(request), characterFrom(request))
+                )
+            }
+            "run" -> {
+                setupEnvironment()
+                Response(OK).with(result of RunHandler.run(projectFrom(request)))
+            }
             else -> Response(Status.NOT_FOUND)
         }
+    }
+    private fun setupEnvironment(){
+        println("Checking tmp directory")
+        val libs = File("/tmp")?.listFiles()?.filter { it.name.endsWith(".jar") }?.toList() ?: emptyList()
+        println("Found the following libs: $libs")
+        if (libs.isEmpty()) {
+            val bucket = "kotlin-playground-server-libs"
+            val client = AmazonS3Client.builder().withRegion("eu-west-1").build()
+            val items = client.listObjects(bucket).objectSummaries
+            items.forEach {
+                println("Downloading ${it.key}")
+                client.getObject(GetObjectRequest(bucket, it.key), File("/tmp/${it.key}"))
+            }
+        }
+        EnvironmentManager.createEnvironment()
     }
     private fun lineFrom(request: Request): Int{
         val lineField = FormField.int().required("line")
